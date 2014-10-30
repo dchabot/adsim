73221f640a0e6e646149564561522304b517db0f
Daron Chabot <dchabot@bnl.gov>
2014-10-30 17:06:14 -0400

    Fix for 64-bit arch.

--- st.cmd
+++ st.cmd
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-SIM_DETECTOR_APP=/usr/lib/epics/bin/linux-x86/simDetectorApp
+SIM_DETECTOR_APP=/usr/lib/epics/bin/linux-x86_64/simDetectorApp
 
 export EPICS_CA_MAX_ARRAY_BYTES=10000000
-export EPICS_CA_AUTO_ADDR_LIST=NO
-export EPICS_CA_ADDR_LIST=10.0.1.255
+#export EPICS_CA_AUTO_ADDR_LIST=NO
+#export EPICS_CA_ADDR_LIST=127.0.0.1
 
 cd iocBoot/iocSimDetector && $SIM_DETECTOR_APP ./st.cmd.linux
