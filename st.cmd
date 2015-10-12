ae72df6bf700afd9ac78e3c86cc6c5f39ac895eb
Daron Chabot <dchabot@bnl.gov>
2015-10-12 16:24:18 -0400

    FIX: path to binary

--- st.cmd
+++ st.cmd
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SIM_DETECTOR_APP=/usr/lib/epics/bin/linux-x86_64/simDetectorApp
+SIM_DETECTOR_APP=/epics/src/areadetector-1-9-1/bin/linux-x86_64/simDetectorApp
 
 export EPICS_CA_MAX_ARRAY_BYTES=10000000
 #export EPICS_CA_AUTO_ADDR_LIST=NO
